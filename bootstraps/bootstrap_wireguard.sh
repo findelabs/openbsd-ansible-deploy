9778ed1e6574473ab360dee3e6bcad672dfc1d49
Verticaleap <circa.1991@gmail.com>
2019-02-17 10:54:55 -0600

    Corrected urls

--- bootstraps/bootstrap_wireguard.sh
+++ bootstraps/bootstrap_wireguard.sh
@@ -1,5 +1,5 @@
 # Bootstrap the system
-ftp -o - https://gitlab.com/Verticaleap/openbsd-wg/raw/master/bootstraps/bootstrap_raw.sh | sh
+ftp -o - https://gitlab.com/Verticaleap/openbsd-ansible-dev/raw/master/bootstraps/bootstrap_raw.sh | sh
 
 # Run playbook
 cd /root/git/openbsd-ansible-dev/ && ansible-playbook install.yml --tags=users,system,pf,wireguard-binaries,wireguard-configs,unbound,vnstatd,ifstated
