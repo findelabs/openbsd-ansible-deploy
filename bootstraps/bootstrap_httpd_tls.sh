54bf22635b7356e323e6929efe8176cdec83c2a4
Verticaleap <circa.1991@gmail.com>
2019-02-17 18:32:38 -0600

    Added httpd bootstraps

---
+++ bootstraps/bootstrap_httpd_tls.sh
@@ -0,0 +1,15 @@
+# DOMAIN must be declared, so that httpd will be configured properly
+if [[ ! -e $DOMAIN ]];
+    echo "DOMAIN must be declared"
+    exit 1
+fi
+
+# Bootstrap the system
+ftp -o - https://gitlab.com/Verticaleap/openbsd-ansible-dev/raw/master/bootstraps/bootstrap_raw.sh | sh
+
+# Set domain variable
+cd /root/git/openbsd-ansible-dev/group_vars
+sed -i.bak "s/ansible_domain.*/ansible_domain: $DOMAIN/g" all
+
+# Run playbook
+cd /root/git/openbsd-ansible-dev/ && ansible-playbook install.yml --tags=users,system,httpd_basic,acme-client,httpd_tls
