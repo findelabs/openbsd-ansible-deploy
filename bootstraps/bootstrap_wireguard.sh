6bf8af789f176c5cfbcfeb7c8bde832d8b10cdf6
Daniel Findell <circa.1991@gmail.com>
2019-02-23 14:19:46 -0600

    Fixed stupid mistake

--- bootstraps/bootstrap_wireguard.sh
+++ bootstraps/bootstrap_wireguard.sh
@@ -2,4 +2,4 @@
 ftp -o - https://gitlab.com/Verticaleap/openbsd-ansible-dev/raw/master/bootstraps/bootstrap_raw.sh | sh
 
 # Run playbook
-cd /root/git/openbsd-ansible-dev/ && ansible-playbook install.yml --tags=users,system,wireguard-pfpf,wireguard-binaries,wireguard-configs,unbound,vnstatd,ifstated
+cd /root/git/openbsd-ansible-dev/ && ansible-playbook install.yml --tags=users,system,wireguard-pf,wireguard-binaries,wireguard-configs,unbound,vnstatd,ifstated
