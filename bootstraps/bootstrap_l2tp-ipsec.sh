d6f86e4af38cf78afc8d85a183cf40c19ed9d6e3
Daniel Findell <circa.1991@gmail.com>
2019-06-09 21:35:31 -0500

    Init commit

---
+++ bootstraps/bootstrap_l2tp-ipsec.sh
@@ -0,0 +1,11 @@
+# Bootstrap the system
+ftp -o - https://gitlab.com/Verticaleap/openbsd-ansible-dev/raw/master/bootstraps/bootstrap_raw.sh | sh
+
+if [[ $USERNAME == "" ]] || [[ $PASSWORD == "" ]] || [[ $PSK == "" ]]; then
+    echo "Please set vars for USERNAME, PASSWORD, and PSK"
+    exit 1
+fi
+
+
+# Run playbook
+cd /root/git/openbsd-ansible-dev/ && ansible-playbook install.yml --tags=users,system,unbound,vnstatd,l2tp-ipsec --extra-vars="user=$USERNAME pass=$PASSWORD psk=$PSK"
