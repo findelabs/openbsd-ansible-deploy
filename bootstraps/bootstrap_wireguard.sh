2aea3de8b6624cf2802ad7fed1a10bdad92e0eb4
Daniel Findell <circa.1991@gmail.com>
2019-06-10 19:40:29 -0500

    added vnstatd var

--- bootstraps/bootstrap_wireguard.sh
+++ bootstraps/bootstrap_wireguard.sh
@@ -2,4 +2,4 @@
 ftp -o - https://gitlab.com/Verticaleap/openbsd-ansible-dev/raw/master/bootstraps/bootstrap_raw.sh | sh
 
 # Run playbook
-cd /root/git/openbsd-ansible-dev/ && ansible-playbook install.yml --tags=users,system,wireguard-pf,wireguard-binaries,wireguard-configs,unbound,vnstatd,ifstated,sysctl_router --extra-vars="unbound_address=10.10.0.1"
+cd /root/git/openbsd-ansible-dev/ && ansible-playbook install.yml --tags=users,system,wireguard-pf,wireguard-binaries,wireguard-configs,unbound,vnstatd,ifstated,sysctl_router --extra-vars="unbound_address=10.10.0.1 vnstatd_interface=tun3"
