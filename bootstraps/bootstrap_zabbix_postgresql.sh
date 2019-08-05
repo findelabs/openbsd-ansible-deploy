04f66098f7da4a9a9c2cdf9e374ac35ca0ecc211
Daniel Findell <circa.1991@gmail.com>
2019-08-04 22:08:32 -0500

    Init commit

---
+++ bootstraps/bootstrap_zabbix_postgresql.sh
@@ -0,0 +1,10 @@
+# Bootstrap the system
+ftp -o - https://gitlab.com/Verticaleap/openbsd-ansible-dev/raw/master/bootstraps/bootstrap_raw.sh | sh
+
+if [[ $ZABBIX_USERNAME == "" ]] || [[ $ZABBIX_PASSWORD == "" ]]; then
+    echo "Please set vars for ZABBIX_USERNAME and ZABBIX_PASSWORD"
+    exit 1
+fi
+
+# Run playbook
+cd /root/git/openbsd-ansible-dev/ && ansible-playbook install.yml --tags=users,system,unbound,zabbix-postgresql --extra-vars="zabbix_user=$ZABBIX_USERNAME zabbix_pass=$ZABBIX_PASSWORD unbound_address=127.0.0.1"
