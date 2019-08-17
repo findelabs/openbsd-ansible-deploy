0aeabe8339d45575c1a480bc5f284e71a66fa8d5
Daniel Findell <circa.1991@gmail.com>
2019-08-17 08:44:40 -0500

    Added ip for localhost instead of string

--- roles/zabbix-web/templates/zabbix.conf.php
+++ roles/zabbix-web/templates/zabbix.conf.php
@@ -11,7 +11,7 @@ $DB['PASSWORD']                 = '{{ zabbix_password }}';
 // Schema name. Used for IBM DB2 and PostgreSQL.
 $DB['SCHEMA']                   = '';
 
-$ZBX_SERVER                             = 'localhost';
+$ZBX_SERVER                             = '127.0.0.1';
 $ZBX_SERVER_PORT                = '10051';
 $ZBX_SERVER_NAME                = '';
 
